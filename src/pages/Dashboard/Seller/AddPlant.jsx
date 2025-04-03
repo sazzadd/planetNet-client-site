234832e604c1e26856472395c290e3afd6bfd05b
sazzad <sazzadador@gmail.com>
2025-04-03 12:30:54 +0600

    handleSubmit AddPlant

--- src/pages/Dashboard/Seller/AddPlant.jsx
+++ src/pages/Dashboard/Seller/AddPlant.jsx
@@ -4,6 +4,12 @@ import AddPlantForm from '../../../components/Form/AddPlantForm'
 const AddPlant = () => {
   const handleSubmit = async e =>{
     e.preventDefault()
+    const form = e.target
+    const name = form.name.value
+    const description = form.description.value
+    const category = form.category.value
+    const price = parseFloat(form.price.value)
+    const quantity =parseFloat(form.quantity.value)
   }
   return (
     <div>
