3892e7c76d7393a02511e4feeaa87112b580e5d2
sazzad <sazzadador@gmail.com>
2025-04-05 01:11:33 +0600

    map card

--- src/components/Home/Plants.jsx
+++ src/components/Home/Plants.jsx
@@ -1,25 +1,32 @@
-import Card from './Card'
-import Container from '../Shared/Container'
-import { useQuery } from '@tanstack/react-query'
-import axios from 'axios'
-import LoadingSpinner from '../Shared/LoadingSpinner'
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import Container from "../Shared/Container";
+import LoadingSpinner from "../Shared/LoadingSpinner";
+import Card from "./Card";
 
 const Plants = () => {
-  const {data:plants, isLoading} = useQuery({
-    queryKey:['plants'],
-    queryFn:async()=> {
-      const {data} = axios(`${import.meta.env.VITE_API_URL}/plants`)
-      return data
-    }
-  })
-  if(isLoading) return <LoadingSpinner></LoadingSpinner>
+  const { data: plants, isLoading } = useQuery({
+    queryKey: ["plants"],
+    queryFn: async () => {
+      const { data } = await axios(`${import.meta.env.VITE_API_URL}/plants`);
+      return data;
+    },
+  });
+  if (isLoading) return <LoadingSpinner></LoadingSpinner>;
   return (
     <Container>
-      <div className='pt-12 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8'>
-        <Card />
-      </div>
+      {plants && plants.length > 0 ? (
+        <div className="pt-12 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
+          {plants.map((plant) => (
+            <Card key={plant._id} plant={plant} />
+          ))}
+          <Card />
+        </div>
+      ) : (
+        <p className="text-center text-2xl">no data available</p>
+      )}
     </Container>
-  )
-}
+  );
+};
 
-export default Plants
+export default Plants;
