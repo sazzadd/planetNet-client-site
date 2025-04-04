6f3d69c105a835ee1e85856d874082182d5007ce
sazzad <sazzadador@gmail.com>
2025-04-05 00:26:54 +0600

    use tanstackQuery for fata fetch for plant

--- src/components/Home/Plants.jsx
+++ src/components/Home/Plants.jsx
@@ -1,7 +1,18 @@
 import Card from './Card'
 import Container from '../Shared/Container'
+import { useQuery } from '@tanstack/react-query'
+import axios from 'axios'
+import LoadingSpinner from '../Shared/LoadingSpinner'
 
 const Plants = () => {
+  const {data:plants, isLoading} = useQuery({
+    queryKey:['plants'],
+    queryFn:async()=> {
+      const {data} = axios(`${import.meta.env.VITE_API_URL}/plants`)
+      return data
+    }
+  })
+  if(isLoading) return <LoadingSpinner></LoadingSpinner>
   return (
     <Container>
       <div className='pt-12 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8'>
--- src/pages/Dashboard/Seller/AddPlant.jsx
+++ src/pages/Dashboard/Seller/AddPlant.jsx
@@ -3,12 +3,17 @@ import { imageUpload } from "../../../api/utils";
 import AddPlantForm from "../../../components/Form/AddPlantForm";
 import useAuth from "./../../../hooks/useAuth";
 import { useState } from "react";
+import useAxiosSecure, { axiosSecure } from './../../../hooks/useAxiosSecure';
+import toast from "react-hot-toast";
 
 const AddPlant = () => {
   const { user } = useAuth();
   const [uploadButtonText,setUploadButtonText] = useState('upload image')
+  const axiosSecure = useAxiosSecure()
+  const [loading,setLoading] = useState(false)
   const handleSubmit = async (e) => {
     e.preventDefault();
+    setLoading(true)
     const form = e.target;
     const name = form.name.value;
     const description = form.description.value;
@@ -34,6 +39,17 @@ const AddPlant = () => {
       seller,
     };
     console.table(planData);
+    try{
+    await axiosSecure.post('/plants', planData)
+    toast.success('data Added Successfully')
+
+    }
+    catch(err){
+      console.log(err)
+    }
+    finally{
+      setLoading(false)
+    }
   };
   return (
     <div>
